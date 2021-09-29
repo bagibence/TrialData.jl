4c38c3365229d3fc7ae2824cdb5d3e4af4ff10bb
Bence Bagi <bagi.bence94@gmail.com>
2021-09-29 16:42:28 +0100

    Forgot to add stretching.jl earlier

---
+++ src/stretching.jl
@@ -0,0 +1,37 @@
+using Interpolations: LinearInterpolation
+
+
+function stretch(arr::AbstractVector, old_anchors, new_anchors)
+    time = new_anchors[1]:new_anchors[end]
+    tt = 1:length(arr)
+
+    stretched = Vector{Float64}(undef, length(time))
+
+    for k in 1:length(new_anchors)-1
+        indtofill = new_anchors[k] .<= time .< new_anchors[k+1]
+
+        compress_factor = (old_anchors[k+1] - old_anchors[k]) / (new_anchors[k+1] - new_anchors[k])
+
+        timeint = old_anchors[k] .+ compress_factor .* (time[indtofill] .- new_anchors[k])
+
+        interp = LinearInterpolation(tt, arr)
+
+        stretched[indtofill] = [interp(t) for t in timeint]
+    end
+    
+    return stretched
+end
+
+
+function stretch(arr::AbstractMatrix, old_anchors, new_anchors)
+    stretched = Matrix{Float64}(undef, length(new_anchors[1]:new_anchors[end]), size(arr, 2))
+    
+    for j in 1:size(arr, 2)
+        stretched[:, j] = stretch(arr[:, j], old_anchors, new_anchors)
+    end
+    
+    return stretched
+    
+    # alternative one-liner
+    #return hcat([stretch(v, get_time_points(trial), new_anchors) for v in eachcol(arr)]...)
+end
