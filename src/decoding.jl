f0eb92b58acf6683b5316c7d37483e23cd698dc5
Bence Bagi <bagi.bence94@gmail.com>
2022-11-10 14:00:32 +0000

    Add option to use cumulative average in decoding through time

--- src/decoding.jl
+++ src/decoding.jl
@@ -1,13 +1,14 @@
 """
-    get_classif_cv_scores_through_time(df, classifier, input_field, out_field, cv = 10; n_jobs = 1)
+$(SIGNATURES)
 
 Get the cross-validation scores through time, trying to classify `out_field` from `input_field` using `classifier`.
 
 # Extra arguments
 - `cv::Integer=10`: number of cross-validation folds
 - `n_jobs::Integer=1`: number of CPU cores to use
+- `cum_av::Bool=false`: use cumulative average of the signal
 """
-function get_classif_cv_scores_through_time(df, classifier, input_field, out_field, cv = 10; n_jobs = 1)
+function get_classif_cv_scores_through_time(df, classifier, input_field, out_field, cv = 10; n_jobs = 1, cum_av = false)
     X_per_trial = permutedims(get_sig_by_trial(df, input_field), (3, 2, 1))
     y = df[!, out_field]
     
@@ -15,7 +16,14 @@ function get_classif_cv_scores_through_time(df, classifier, input_field, out_fie
     
     cv_scores = []
     for t in 1:T
-        push!(cv_scores, model_selection.cross_val_score(classifier, X_per_trial[:, :, t], y, cv = model_selection.StratifiedKFold(cv, shuffle=true), n_jobs = n_jobs))
+        if cum_av
+          x = dropdims(mean(X_per_trial[:, :, 1:t],
+                            dims = 3),
+                       dims = 3)
+        else
+          x = X_per_trial[:, :, t]
+        end
+        push!(cv_scores, model_selection.cross_val_score(classifier, x, y, cv = model_selection.StratifiedKFold(cv, shuffle=true), n_jobs = n_jobs))
     end
     
     return hcat(cv_scores...)
@@ -23,7 +31,7 @@ end
 
 
 """
-    get_regr_cv_scores_through_time(df, regressor, input_field, out_field, cv = 10; n_jobs=1, scoring=sk_metrics.make_scorer(sk_metrics.r2_score, multioutput = "variance_weighted"))
+$(SIGNATURES)
 
 Get the cross-validation scores through time, trying to regress onto `out_field` from `input_field` using `regressor`.
 Returns a `cv x T` array.
@@ -32,8 +40,9 @@ Returns a `cv x T` array.
 - `cv::Integer=10`: number of cross-validation folds
 - `n_jobs::Integer=1`: number of CPU cores to use
 - `scoring`: sklearn scorer to use. Defaults to variance weighted R2
+- `cum_av::Bool=false`: use cumulative average of the signal
 """
-function get_regr_cv_scores_through_time(df, regressor, input_field, out_field, cv = 10; n_jobs=1, scoring=sk_metrics.make_scorer(sk_metrics.r2_score, multioutput = "variance_weighted"))
+function get_regr_cv_scores_through_time(df, regressor, input_field, out_field, cv = 10; n_jobs=1, scoring=sk_metrics.make_scorer(sk_metrics.r2_score, multioutput = "variance_weighted"), cum_av = false)
     X_per_trial = permutedims(get_sig_by_trial(df, input_field), (3, 2, 1))
     y = df[!, out_field]
     
@@ -41,7 +50,14 @@ function get_regr_cv_scores_through_time(df, regressor, input_field, out_field, 
     
     cv_scores = []
     for t in 1:T
-        push!(cv_scores, model_selection.cross_val_score(regressor, X_per_trial[:, :, t], y, cv = model_selection.KFold(cv, shuffle=true), scoring = scoring, n_jobs = n_jobs))
+        if cum_av
+          x = dropdims(mean(X_per_trial[:, :, 1:t],
+                            dims = 3),
+                       dims = 3)
+        else
+          x = X_per_trial[:, :, t]
+        end
+        push!(cv_scores, model_selection.cross_val_score(regressor, x, y, cv = model_selection.KFold(cv, shuffle=true), scoring = scoring, n_jobs = n_jobs))
     end
 
     
@@ -50,14 +66,15 @@ end
 
 
 """
-    get_predictive_axis_through_time(df, predictor, input_field, out_field)
+$(SIGNATURES)
 
 Fit `predictor` at every time point, predicting `out_field` from `input_field` and record the axes used for prediction.
 `predictor` has to have a `.coef_` field.
+If `cum_av` is true, use a cumulative average of the signal.
 
 Returns an array of coefficients (which are arrays themselves).
 """
-function get_predictive_axis_through_time(df, predictor, input_field, out_field)
+function get_predictive_axis_through_time(df, predictor, input_field, out_field; cum_av = false)
     X_per_trial = permutedims(get_sig_by_trial(df, input_field), (3, 2, 1))
     y = df[!, out_field]
     
@@ -65,6 +82,13 @@ function get_predictive_axis_through_time(df, predictor, input_field, out_field)
     
     coefficients = []
     for t in 1:T
+        if cum_av
+          x = dropdims(mean(X_per_trial[:, :, 1:t],
+                            dims = 3),
+                       dims = 3)
+        else
+          x = X_per_trial[:, :, t]
+        end
         push!(coefficients, predictor.fit(X_per_trial[:, :, t], y).coef_)
     end
 
